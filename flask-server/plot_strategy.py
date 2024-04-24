88eada2a2de7a520cb4f908b3ee3d02cdb09bbbf
Melih <melih.demirel@student.uhasselt.be>
2024-04-24 15:23:41 +0200

    x

--- flask-server/plot_strategy.py
+++ flask-server/plot_strategy.py
@@ -5,6 +5,7 @@ Tyre strategies during a race
 Plot all drivers' tyre strategies during a race.
 """
 
+import json
 from matplotlib import pyplot as plt
 
 import fastf1
@@ -21,15 +22,38 @@ laps = session.laps
 ###############################################################################
 # Get the list of driver numbers
 drivers = session.drivers
-print("printing drivers")
-print(drivers)
-print(session.get_driver(drivers[0]))
 
+print(drivers)
 ###############################################################################
 # Convert the driver numbers to three letter abbreviations
+
+drivers_list = [{session.get_driver(driver)["Abbreviation"]: session.get_driver(driver)["DriverId"]} for driver in drivers]
+driversL = {}
+for driver in drivers:
+    driversL[session.get_driver(driver)["Abbreviation"]] = session.get_driver(driver)["DriverId"]
+
+print(driversL)
+print(drivers_list)
+
+# Create a dictionary from the list of dictionaries
+drivers_dict = {list(d.keys())[0]: list(d.values())[0] for d in drivers_list}
+
+# Function to get driver name by code
+def get_driver_name(driver_code):
+    if driver_code in drivers_dict:
+        return drivers_dict[driver_code]
+    else:
+        return None
+
+# Example usage
+driver_code = "HAM"
+driver_name = get_driver_name(driver_code)
+if driver_name:
+    print(f"The driver name for code {driver_code} is {driver_name}.")
+else:
+    print(f"No driver found with code {driver_code}.")
+
 drivers = [session.get_driver(driver)["Abbreviation"] for driver in drivers]
-print("printing code")
-print(drivers)
 
 # print(session.weather_data)
 
@@ -39,16 +63,23 @@ print(drivers)
 # We do this by first grouping the laps by the driver,
 # the stint number, and the compound.
 # And then counting the number of laps in each group.
-print(laps)
 stints = laps[["Driver", "Stint", "Compound", "LapNumber"]]
 stints = stints.groupby(["Driver", "Stint", "Compound"])
 stints = stints.count().reset_index()
-
 ###############################################################################
 # The number in the LapNumber column now stands for the number of observations
 # in that group aka the stint length.
 stints = stints.rename(columns={"LapNumber": "StintLength"})
-print(stints)
+# Grouping by Driver and creating lists of compounds and stint lengths
+driver_data = stints.groupby('Driver').apply(lambda x: {
+    'tires': x['Compound'].tolist(),
+    'laps': x['StintLength'].tolist()
+}).to_dict()
+
+# Converting to the desired JSON format
+json_data = {driversL[driver]: {'code': driver, 'tires': data['tires'], 'laps': data['laps']} for driver, data in driver_data.items()}
+
+print(json_data)
 
 ###############################################################################
 # Now we can plot the strategies for each driver
